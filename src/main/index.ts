d0980df6f7062e5a4f47743cfd96219aacb8c7e3
Vojtech Nerad <vojtech.nerad2@gmail.com>
2026-09-16 02:22:27 +0200

    Configure MOWL Identity

--- src/main/index.ts
+++ src/main/index.ts
@@ -8,6 +8,7 @@ function createWindow(): void {
   const mainWindow = new BrowserWindow({
     width: 900,
     height: 670,
+    title: 'MOWL',
     show: false,
     autoHideMenuBar: true,
     ...(process.platform === 'linux' ? { icon } : {}),
@@ -40,7 +41,7 @@ function createWindow(): void {
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   // Set app user model id for windows
-  electronApp.setAppUserModelId('com.electron')
+  electronApp.setAppUserModelId('com.mowl.app')
 
   // Default open or close DevTools by F12 in development
   // and ignore CommandOrControl + R in production.
